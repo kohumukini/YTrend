ff5a64289887862c3c051d0a1cf6e20554f45a72
kohumukini <kealii.ohumukini@gmail.com>
2026-06-02 22:23:41 -0700

    Building out components & skeleton of frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,31 @@
 import './index.css';
 import Header from "./components/Header"; 
+import Toolbar from "./components/Toolbar"
 
 function App() {
   return (
-    <Header />
+    <div className="h-screen bg-black text-white">
+      <Header />
+      <main className="flex flex-row h-[60%] border-b border-white/10">
+        <div className="basis-3/4 m-4 p-8">
+          <Toolbar />
+          <div className="bg-[#141418] w-full h-[80%] my-4">
+            Chart
+          </div>
+        </div>
+        <aside className="basis-1/4 bg-[#141418]">
+          <div>
+            Sidebar
+          </div>
+        </aside>
+      </main>
+      <div>
+        Stats
+      </div>
+      <footer className="bg-[#141418] bottom-0 fixed w-full p-4 border-t border-white/10">
+        All Rights Reserved
+      </footer>
+    </div>  
   )
 }
 
--- frontend/src/components/Toolbar.tsx
+++ frontend/src/components/Toolbar.tsx
@@ -1,8 +1,21 @@
 const Toolbar = () => {
     return (
-        <section className="w-full flex flex-row justify-between items-center">
-            
-        </section>
+        <div className="w-full flex flex-row justify-between items-center">
+            <section className="flex flex-row justify-between gap-4 items-center ">
+                <div className="border-[0.5px] border-[#FF6B2B] p-2 rounded-xl bg-[#212124]">
+                    AAPL 
+                    <button className="text-[#ff6b2bca] bg-[#ff6b2b3c] py-.5 px-2 mx-2 my-1 rounded-lg border border-[#ff6b2b] text-center">
+                        +2
+                    </button>
+                </div>
+                <div className="border-[0.5px] border-white/5 p-2 rounded-xl bg-[#212124]">+ Add ticker</div>
+            </section>
+            <section>
+                <button className="border-2 border-white/5 font-semibold rounded-lg py-2 px-4 cursor-pointer bg-[#FF6B2B] text-white shadow-[0_0_15px_5px_rgba(255,107,43,0.4)]">
+                    ▷ Run
+                </button>
+            </section>
+        </div>
     )
 }
 
